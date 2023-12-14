438ec661ad6689c1e99d8f2c02ce11dfed933225
Jille342 <151497575+Jille342@users.noreply.github.com>
2023-12-14 14:29:46 +0900

    Update NameTags2.java

--- src/main/java/com/github/satellite/features/module/render/NameTags2.java
+++ src/main/java/com/github/satellite/features/module/render/NameTags2.java
@@ -15,6 +15,8 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.text.TextFormatting;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
@@ -33,6 +35,7 @@ public class NameTags2 extends Module {
     private final CFontRenderer font = Fonts.default18;
     BooleanSetting renderSelf;
     BooleanSetting showItems;
+    BooleanSetting ench;
     public NameTags2() {
         super("NameTags2", 0, Category.RENDER);
     }
@@ -40,7 +43,8 @@ public class NameTags2 extends Module {
     public void init() {
         renderSelf = new BooleanSetting("Render Self", false);
         showItems = new BooleanSetting("Show Items", true);
-        addSetting(renderSelf, showItems);
+        ench = new BooleanSetting("show Enchant", true);
+       addSetting(renderSelf, showItems, ench);
         super.init();
     }
 
@@ -52,14 +56,13 @@ public class NameTags2 extends Module {
                 if (player == null) continue;
                 if (player.deathTime > 0) continue;
                 if (player == mc.player  && !renderSelf.enable) continue;
-                double x = ((player.lastTickPosX + (player.posX - player.lastTickPosX)
+                double x = ((player.lastTickPosX + (player.posX - player.lastTickPosX) * mc.getRenderPartialTicks()
                         - mc.getRenderManager().viewerPosX));
-                double y = ((player.lastTickPosY + (player.posY - player.lastTickPosY)
+                double y = ((player.lastTickPosY + (player.posY - player.lastTickPosY) * mc.getRenderPartialTicks()
                         - mc.getRenderManager().viewerPosY));
-                double z = ((player.lastTickPosZ + (player.posZ - player.lastTickPosZ)
+                double z = ((player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * mc.getRenderPartialTicks()
                         - mc.getRenderManager().viewerPosZ));
                 renderTag(player, x, y, z);
-
             }
         }
     }
@@ -73,24 +76,9 @@ public class NameTags2 extends Module {
     }
 
 
-
-    private void renderEnchants(ItemStack itemStack, int posX, int posY) {
-        GlStateManager.enableTexture2D();
-
-        for (Enchantment enchantment : EnchantmentHelper.getEnchantments(itemStack).keySet()) {
-            if (enchantment == null) {
-                continue;
-            }
-
-            posY += 8;
-        }
-
-
-        GlStateManager.disableTexture2D();
-    }
-    private void renderTag(Entity entity, double x, double y, double z) {
+    private void renderTag(EntityPlayer entity, double x, double y, double z) {
         double dist = mc.player.getDistance(x, y, z);
-        double scale = 1;
+        double scale ;
         String name = entity.getDisplayName().getFormattedText();
         name = name.replace(entity.getDisplayName().getFormattedText(), "\247f" + entity.getDisplayName().getFormattedText());
 
@@ -100,8 +88,8 @@ public class NameTags2 extends Module {
         if (entity instanceof EntityLivingBase) {
             name = name + " \247a" + ((double)Math.round((((EntityLivingBase) entity).getHealth() * 100) / 100) / 2);
         }
-        scale = 0.0018 + 0.0001 * dist;
-        if (dist <= 8.0) scale = 0.000245;
+        scale = 0.00018 + 0.00015 * dist;
+        if (dist <= 8.0) scale = 0.0245;
 
         float var13 = 1.6F;
 
@@ -135,6 +123,28 @@ public class NameTags2 extends Module {
 
         font.drawStringWithShadow(name, -var17, var16, 0xFFFFFF);
 
+        if (this.showItems.enable) {
+            GlStateManager.pushMatrix();
+            int xOffset = -8;
+            for (ItemStack stack : entity.inventory.armorInventory) {
+                if (stack == null) continue;
+                xOffset -= 8;
+            }
+            xOffset -= 8;
+            ItemStack renderOffhand = entity.getHeldItemOffhand().copy();
+            this.renderItemStack(renderOffhand, xOffset, -26);
+            xOffset += 16;
+            for (ItemStack stack : entity.inventory.armorInventory) {
+                if (stack == null) continue;
+                ItemStack armourStack = stack.copy();
+                this.renderItemStack(armourStack, xOffset, -26);
+                xOffset += 16;
+            }
+            ItemStack renderMainHand = entity.getHeldItemMainhand().copy();
+            this.renderItemStack(renderMainHand, xOffset, -26);
+            GlStateManager.popMatrix();
+        }
+
         mc.entityRenderer.disableLightmap();
         GL11.glLineWidth(1.0F);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
@@ -144,6 +154,7 @@ public class NameTags2 extends Module {
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glDepthMask(true);
 
+
         GlStateManager.enableDepth();
         GlStateManager.depthMask(true);
         GlStateManager.enableTexture2D();
@@ -151,6 +162,44 @@ public class NameTags2 extends Module {
         GlStateManager.enableLighting();
         GlStateManager.popMatrix();
     }
+    private void renderItemStack(ItemStack stack, int x, int y) {
+        GlStateManager.depthMask(true);
+        GlStateManager.disableBlend();
+        mc.getRenderItem().zLevel = -150.0f;
+        GlStateManager.disableAlpha();
+        GlStateManager.disableCull();
+        RenderHelper.enableGUIStandardItemLighting();
+        mc.getRenderItem().renderItemAndEffectIntoGUI(stack, x, y);
+        mc.getRenderItem().renderItemOverlays(mc.fontRenderer, stack, x, y);
+        mc.getRenderItem().zLevel = 0.0f;
+        RenderHelper.disableStandardItemLighting();
+           GlStateManager.enableCull();
+         GlStateManager.enableAlpha();
+        GL11.glScaled(0.5f, 0.5f, 0.5f);
+          GlStateManager.disableDepth();
+        if (ench.enable)
+            this.renderEnchantmentText(stack, x, y);
+        GL11.glScaled(2.0f, 2.0f, 2.0f);
+        GlStateManager.enableBlend();
+
+    }
+
+    private void renderEnchantmentText(ItemStack stack, int x, int y) {
+        int enchantmentY = y - 8;
+        NBTTagList enchants = stack.getEnchantmentTagList();
+        for (int index = 0; index < enchants.tagCount(); ++index) {
+            short id = enchants.getCompoundTagAt(index).getShort("id");
+            short level = enchants.getCompoundTagAt(index).getShort("lvl");
+            Enchantment enc = Enchantment.getEnchantmentByID(id);
+            if (enc == null) continue;
+            String encName = enc.isCurse() ? TextFormatting.RED + enc.getTranslatedName(level).substring(11).substring(0, 1).toLowerCase() : enc.getTranslatedName(level).substring(0, 1).toLowerCase();
+            encName = encName + level;
+           font.drawStringWithShadow(encName, x * 2, enchantmentY, -1);
+            enchantmentY -= 8;
+        }
+
+    }
+
 
 
     public static void drawItem(ItemStack itemstack, int i, int j)
